6376468621acd63d3538a1dc80effdb696b29bc5
baitaoPink <yixiang374@gmail.com>
2025-05-05 17:27:58 +0800

    upd: 更新

---
+++ admin/assets/report-CyHjnHVq.js
@@ -0,0 +1,1 @@
+import{a0 as s}from"./index-B8uXi1CT.js";function e(t){return s({url:"/system/system/data_statistics/list",params:t})}function i(t){return s({url:"/system/system/data_statistics/home ",params:t})}export{e as a,i as g};
