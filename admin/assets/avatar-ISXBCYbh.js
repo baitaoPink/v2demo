51a99e86523666a40f34fca960b99ec882846533
baitaoPink <yixiang374@gmail.com>
2025-05-05 15:06:25 +0800

    upd: 更新

--- admin/assets/avatar-ISXBCYbh.js
+++
@@ -1,1 +0,0 @@
-const a="/v2demo/admin/assets/avatar-C3nF7gXh.svg";export{a as default};
---
+++ admin/assets/report-CPHsXpfs.js
@@ -0,0 +1,1 @@
+import{a0 as s}from"./index-BGn2hxht.js";function e(t){return s({url:"/system/system/data_statistics/list",params:t})}function i(t){return s({url:"/system/system/data_statistics/home ",params:t})}export{e as a,i as g};
